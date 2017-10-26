e0fc88fa871c516a50d0422a3d43a30669a3f3b3
myronchang <myronchang@users.noreply.github.com>
2017-10-25 21:54:30 -0400

    Added for-loops and new DFs
    Four new for-loops for easier checking of which columns have missing values, and putting cleaned datasets into separate data frames.

--- DataCleaning.R
+++ DataCleaning.R
@@ -17,29 +17,74 @@ mode <- function(x){
     return(uniqx[which.max(tabulate(match(x, uniqx)))])
   }
 }
-train$ps_ind_02_cat[train$ps_ind_02_cat == -1] <- mode(train$ps_ind_02_cat)
-train$ps_ind_04_cat[train$ps_ind_04_cat == -1] <- mode(train$ps_ind_04_cat)
-train$ps_ind_05_cat[train$ps_ind_05_cat == -1] <- mode(train$ps_ind_05_cat)
-train$ps_reg_03[train$ps_reg_03 == -1] <- mean(train$ps_reg_03)
-train$ps_car_01_cat[train$ps_car_01_cat == -1] <- mode(train$ps_car_01_cat)
-train$ps_car_02_cat[train$ps_car_02_cat == -1] <- mode(train$ps_car_02_cat)
-train$ps_car_03_cat[train$ps_car_03_cat == -1] <- mode(train$ps_car_03_cat)
-train$ps_car_05_cat[train$ps_car_05_cat == -1] <- mode(train$ps_car_05_cat)
-train$ps_car_07_cat[train$ps_car_07_cat == -1] <- mode(train$ps_car_07_cat)
-train$ps_car_09_cat[train$ps_car_09_cat == -1] <- mode(train$ps_car_09_cat)
-train$ps_car_11[train$ps_car_11 == -1] <- mode(train$ps_car_11)
-train$ps_car_12[train$ps_car_12 == -1] <- mean(train$ps_car_12)
-train$ps_car_14[train$ps_car_14 == -1] <- mean(train$ps_car_14)
 
-test$ps_ind_02_cat[test$ps_ind_02_cat == -1] <- mode(test$ps_ind_02_cat)
-test$ps_ind_04_cat[test$ps_ind_04_cat == -1] <- mode(test$ps_ind_04_cat)
-test$ps_ind_05_cat[test$ps_ind_05_cat == -1] <- mode(test$ps_ind_05_cat)
-test$ps_reg_03[test$ps_reg_03 == -1] <- mean(test$ps_reg_03)
-test$ps_car_01_cat[test$ps_car_01_cat == -1] <- mode(test$ps_car_01_cat)
-test$ps_car_02_cat[test$ps_car_02_cat == -1] <- mode(test$ps_car_02_cat)
-test$ps_car_03_cat[test$ps_car_03_cat == -1] <- mode(test$ps_car_03_cat)
-test$ps_car_05_cat[test$ps_car_05_cat == -1] <- mode(test$ps_car_05_cat)
-test$ps_car_07_cat[test$ps_car_07_cat == -1] <- mode(test$ps_car_07_cat)
-test$ps_car_09_cat[test$ps_car_09_cat == -1] <- mode(test$ps_car_09_cat)
-test$ps_car_11[test$ps_car_11 == -1] <- mode(test$ps_car_11)
-test$ps_car_14[test$ps_car_14 == -1] <- mean(test$ps_car_14)
+# Check which columns have missing values
+# Training data
+for (i in 3:59){
+  missingvalues <- sum(train[,i] == -1)
+  if (missingvalues > 0){
+    columnname <- colnames(train[i])
+    print(columnname)
+    print(missingvalues)
+  }
+}
+# Testing data
+for (i in 2:58){
+  missingvalues <- sum(test[,i] == -1)
+  if (missingvalues > 0){
+    columnname <- colnames(test[i])
+    print(columnname)
+    print(missingvalues)
+  }
+}
+
+# Create copies of the raw train and test data
+train_clean <- train
+test_clean <- test
+
+train_clean$ps_ind_02_cat[train_clean$ps_ind_02_cat == -1] <- mode(train_clean$ps_ind_02_cat)
+train_clean$ps_ind_04_cat[train_clean$ps_ind_04_cat == -1] <- mode(train_clean$ps_ind_04_cat)
+train_clean$ps_ind_05_cat[train_clean$ps_ind_05_cat == -1] <- mode(train_clean$ps_ind_05_cat)
+train_clean$ps_reg_03[train_clean$ps_reg_03 == -1] <- mean(train_clean$ps_reg_03)
+train_clean$ps_car_01_cat[train_clean$ps_car_01_cat == -1] <- mode(train_clean$ps_car_01_cat)
+train_clean$ps_car_02_cat[train_clean$ps_car_02_cat == -1] <- mode(train_clean$ps_car_02_cat)
+train_clean$ps_car_03_cat[train_clean$ps_car_03_cat == -1] <- mode(train_clean$ps_car_03_cat)
+train_clean$ps_car_05_cat[train_clean$ps_car_05_cat == -1] <- mode(train_clean$ps_car_05_cat)
+train_clean$ps_car_07_cat[train_clean$ps_car_07_cat == -1] <- mode(train_clean$ps_car_07_cat)
+train_clean$ps_car_09_cat[train_clean$ps_car_09_cat == -1] <- mode(train_clean$ps_car_09_cat)
+train_clean$ps_car_11[train_clean$ps_car_11 == -1] <- mode(train_clean$ps_car_11)
+train_clean$ps_car_12[train_clean$ps_car_12 == -1] <- mean(train_clean$ps_car_12)
+train_clean$ps_car_14[train_clean$ps_car_14 == -1] <- mean(train_clean$ps_car_14)
+
+test_clean$ps_ind_02_cat[test_clean$ps_ind_02_cat == -1] <- mode(test_clean$ps_ind_02_cat)
+test_clean$ps_ind_04_cat[test_clean$ps_ind_04_cat == -1] <- mode(test_clean$ps_ind_04_cat)
+test_clean$ps_ind_05_cat[test_clean$ps_ind_05_cat == -1] <- mode(test_clean$ps_ind_05_cat)
+test_clean$ps_reg_03[test_clean$ps_reg_03 == -1] <- mean(test_clean$ps_reg_03)
+test_clean$ps_car_01_cat[test_clean$ps_car_01_cat == -1] <- mode(test_clean$ps_car_01_cat)
+test_clean$ps_car_02_cat[test_clean$ps_car_02_cat == -1] <- mode(test_clean$ps_car_02_cat)
+test_clean$ps_car_03_cat[test_clean$ps_car_03_cat == -1] <- mode(test_clean$ps_car_03_cat)
+test_clean$ps_car_05_cat[test_clean$ps_car_05_cat == -1] <- mode(test_clean$ps_car_05_cat)
+test_clean$ps_car_07_cat[test_clean$ps_car_07_cat == -1] <- mode(test_clean$ps_car_07_cat)
+test_clean$ps_car_09_cat[test_clean$ps_car_09_cat == -1] <- mode(test_clean$ps_car_09_cat)
+test_clean$ps_car_11[test_clean$ps_car_11 == -1] <- mode(test_clean$ps_car_11)
+test_clean$ps_car_14[test_clean$ps_car_14 == -1] <- mean(test_clean$ps_car_14)
+
+# Check to make sure all missing values have been addressed
+# Training data
+for (i in 3:59){
+  missingvalues <- sum(train_clean[,i] == -1)
+  if (missingvalues > 0){
+    columnname <- colnames(train_clean[i])
+    print(columnname)
+    print(missingvalues)
+  }
+}
+# Testing data
+for (i in 2:58){
+  missingvalues <- sum(test_clean[,i] == -1)
+  if (missingvalues > 0){
+    columnname <- colnames(test_clean[i])
+    print(columnname)
+    print(missingvalues)
+  }
+}
